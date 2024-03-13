6caa2bd45de9ff8de14bbf682af0a3a8b6fd704b
Your Name <thibaultcourcol@gmail.com>
2024-03-13 22:23:42 +0100

    update

--- src/components/AppShop.vue
+++ src/components/AppShop.vue
@@ -1,69 +1,229 @@
 <template>
   <div>
-    <!-- Contenu de votre composant -->
-    <button @click="fetchData('shoes')">Rechercher des chaussures</button>
-    <button @click="fetchData('t-shirt')">Rechercher des t-shirts</button>
+    <div class="form-container">
+      <form @submit.prevent="addProduct" class="product-form">
+        <input type="text" v-model="name" placeholder="Nom du produit" class="form-input">
+        <input type="text" v-model="image" placeholder="URL de l'image" class="form-input">
+        <input type="text" v-model="price" placeholder="Prix du produit" class="form-input">
+        <select v-model="category" class="form-input">
+          <option disabled value="">Veuillez sélectionner une catégorie</option>
+          <option>Jeans</option>
+          <option>T-shirt</option>
+          <option>Chaussette</option>
+          <option>Chaussure</option>
+          <!-- Ajoutez d'autres options de catégorie ici -->
+        </select>
+        <button type="submit" class="form-button">Ajouter le produit</button>
+      </form>
+      <div v-if="message" class="message">{{ message }}</div>
+    </div>
 
-    <template v-if="responseData">
-      <div v-for="(product, index) in responseData.searchProductDetails.slice(0, 3)" :key="index">
-        <h2>{{ product.productDescription }}</h2>
-        <img :src="product.imgUrl" alt="Image du produit">
-        <p>Prix : {{ product.price }} $</p>
-        <p>ASIN : {{ product.asin }}</p>
+    <!-- Carrousel de produits -->
+    <div ref="productsSection" class="products-container">
+      <div v-for="product in products" :key="product.name" class="product-item">
+        <img :src="product.image" :alt="product.name" class="product-image" @load="scrollToProduct">
+        <div>{{ product.name }}</div>
+        <div>{{ product.price }}</div>
       </div>
-    </template>
+    </div>
+
+    <!-- Boutons de catégorie -->
+    <div class="button-container">
+      <button v-for="category in categories" :key="category" class="category-button" @click="fetchProducts(category)">
+        {{ category }}
+      </button>
+    </div>
+
   </div>
 </template>
 
-<!-- Rest of your code -->
-
 <script>
-import axios from 'axios';
+import { ref, nextTick, watch } from 'vue'
+import { getDatabase, ref as dbRef, set, onValue } from "firebase/database";
 
 export default {
-  data() {
-    return {
-      responseData: null,
-      cache: {} // Ajout de la propriété cache
-    };
-  },
-  methods: {
-    fetchData(keyword) {
-      // Vérifiez si les résultats pour le mot-clé sont dans le cache
-      if (this.cache[keyword]) {
-        this.responseData = this.cache[keyword];
-        return;
+  setup() {
+    const name = ref('');
+    const image = ref('');
+    const price = ref('');
+    const category = ref(''); // Ajout d'une référence pour la catégorie
+    const categories = ref(['Jeans', 'T-shirt', 'Chaussette', 'Chaussure']);
+    const products = ref([]);
+    const message = ref('');
+
+    const addProduct = async () => {
+      // Vérifier si les champs sont vides
+      if (!category.value) {
+        // Afficher un message d'erreur
+        message.value = 'Veuillez sélectionner une catégorie.';
+        return; // Arrêter l'exécution de la méthode
       }
 
-      const options = {
-        method: 'GET',
-        url: `https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-search-by-keyword-asin`,
-        params: {
-          domainCode: 'com',
-          keyword: keyword,
-          page: '1',
-          excludeSponsored: 'false',
-          sortBy: 'relevanceblender',
-          withCache: 'true'
-        },
-        headers: {
-          'X-RapidAPI-Key': process.env.VUE_APP_RAPIDAPI_KEY,
-          'X-RapidAPI-Host': 'axesso-axesso-amazon-data-service-v1.p.rapidapi.com'
-        }
-      };
+      // Obtenir une référence à la base de données Firebase
+      const db = getDatabase();
 
-      axios.request(options).then(response => {
-        this.responseData = response.data;
-        // Stockez les résultats dans le cache
-        this.cache[keyword] = response.data;
-        console.log(this.responseData);
-      }).catch(error => {
-        console.error(error);
+      // Créer un nouvel objet dans Firebase avec les détails du produit
+      const productRef = dbRef(db, `products/${category.value}/${name.value}`); // Utilisation de la catégorie comme clé parente
+      set(productRef, {
+        name: name.value,
+        image: image.value,
+        price: price.value
       });
-    }
+      // Réinitialiser les valeurs du formulaire
+      name.value = '';
+      image.value = '';
+      price.value = '';
+      category.value = '';
+
+      // Afficher le message
+      message.value = 'Le produit a été ajouté avec succès !';
+
+      // Effacer le message après 3 secondes
+      setTimeout(() => {
+        message.value = '';
+      }, 3000);
+    };
+
+    const fetchProducts = async (category) => {
+      const db = getDatabase();
+      const productRef = dbRef(db, `products/${category}`);
+      onValue(productRef, (snapshot) => {
+        products.value = snapshot.val();
+      });
+    };
+
+    const scrollToProduct = () => {
+      nextTick(() => {
+        const firstProductImage = document.querySelector('.product-image');
+        if (firstProductImage) {
+          firstProductImage.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    };
+
+
+
+    watch(products, () => {
+      // Scroll to the products section after products are updated
+      nextTick(() => {
+        const firstProductImage = document.querySelector('.product-image');
+        if (firstProductImage) {
+          firstProductImage.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+
+
+    return {name, image, price, category, categories, products, addProduct, fetchProducts, scrollToProduct, message};
   }
 };
 </script>
-<style scoped>
 
+<style scoped>
+.products-container {
+  display: flex;
+  flex-direction: row; /* Change this to row */
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.category-button {
+  padding: 10px 20px;
+  background-color: #4CAF50; /* Vous pouvez changer la couleur de fond */
+  border: none;
+  border-radius: 5px;
+  color: white; /* Vous pouvez changer la couleur du texte */
+  font-size: 16px; /* Vous pouvez changer la taille du texte */
+  cursor: pointer;
+  margin: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.category-button:hover {
+  background-color: #45a049; /* Vous pouvez changer la couleur de fond au survol */
+}
+
+.product-image {
+  max-width: 100%;
+  max-height: 200px; /* Ajustez cette valeur selon vos besoins */
+  object-fit: cover;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.product-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #f8f9fa;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.form-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ced4da;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+.form-button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.form-button:hover {
+  background-color: #0056b3;
+}
+
+.products-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.product-item {
+  margin: 10px;
+}
+
+.product-image {
+  max-width: 100%;
+  max-height: 200px;
+  object-fit: cover;
+}
+
+.message {
+  margin-top: 20px;
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border-radius: 5px;
+  transition: opacity 1s ease-out;
+}
+
+.message[hidden] {
+  opacity: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './bootstrap/dist/css/bootstrap.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel, Slide } from 'vue3-carousel';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { initializeApp } from "firebase/app";
+import 'firebase/database';
 
-createApp(App).mount('#app')
+// Votre configuration Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAJRp8WxcFUDzBzsRnqG-TZTUQG3jWGjnQ",
+    authDomain: "vuejs-ae8a0.firebaseapp.com",
+    databaseURL: "https://vuejs-ae8a0-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "vuejs-ae8a0",
+    storageBucket: "vuejs-ae8a0.appspot.com",
+    messagingSenderId: "104652181195",
+    appId: "1:104652181195:web:9bba7498e6fb88baa7d54e",
+    measurementId: "G-2CJPHT3X6K"
+};
+
+// Initialisation de Firebase
+initializeApp(firebaseConfig);
+
+const app = createApp(App);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Carousel', Carousel);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Slide', Slide);
+app.mount('#app');
