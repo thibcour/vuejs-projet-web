b13cc710f729fb197531404d8b3839e6270dc294
Your Name <thibaultcourcol@gmail.com>
2024-03-13 17:37:03 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/vuejs-projet-web/'
+      : '/',
   transpileDependencies: true,
   devServer: {
     https: true,
