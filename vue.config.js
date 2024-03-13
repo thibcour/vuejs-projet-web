9d8be5ad70c32d475e9cde3dcccd2f1c5e52fc2a
Tibo_Courcol <44385130+thibcour@users.noreply.github.com>
2024-03-13 14:15:24 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/repo/'
+      : '/',
   transpileDependencies: true,
   devServer: {
     https: true,
